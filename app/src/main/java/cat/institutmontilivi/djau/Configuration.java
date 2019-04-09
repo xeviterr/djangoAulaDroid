4e69b785161cf7b5d4c82db86889d4a46a455f27
Pebio <xeviterr@gmail.com>
2019-04-09 11:30:38 +0200

    canvis presentació

--- app/src/main/java/cat/institutmontilivi/djau/Configuration.java
+++ app/src/main/java/cat/institutmontilivi/djau/Configuration.java
@@ -1,10 +1,11 @@
-package com.example.djau;
+package cat.institutmontilivi.djau;
 
 public class Configuration
 {
     private static Configuration instance;
 
     public String APILevel="1";
+    public boolean APIDebug=true;
 
     public static Configuration getInstance()
     {
